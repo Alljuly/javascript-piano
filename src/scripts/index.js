e49e8d7035c8b061bb7534f41f898adfe0d1382e
Alljuly <ajsc.elsie@gmail.com>
2024-01-05 14:33:03 +0000

    som ao clicar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,17 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
+const audio = new Audio("../tunes/a.wav")
+
+
+
+const playtune = (key) =>{
+    audio.src = `../tunes/${key}.wav`
+    audio.play()
+}
+
+
+
+pianoKeys.forEach( (key) =>{
+    key.addEventListener("click", () => playtune(key.dataset.key))
+})
+
