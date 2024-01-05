a5c472e3fd9dcc1edac1a0223156fc78fcaf6a4e
Alljuly <ajsc.elsie@gmail.com>
2024-01-05 14:58:27 +0000

    add keydown

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,17 +1,29 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
 
 const audio = new Audio("../tunes/a.wav")
+let mapedKeys = []
 
 
 
 const playtune = (key) =>{
     audio.src = `../tunes/${key}.wav`
     audio.play()
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`)
+
+    clickedKey.classList.add("active")
+    setTimeout(() => {
+        clickedKey.classList.remove("active", 150)
+    })
 }
 
-
-
-pianoKeys.forEach( (key) =>{
+pianoKeys.forEach( (key) => {
     key.addEventListener("click", () => playtune(key.dataset.key))
+    mapedKeys.push(key.dataset.key)
 })
 
+document.addEventListener("keydown", (e) => {
+    if( mapedKeys.includes(e.key)){
+        playtune(e.key)
+    } 
+})
